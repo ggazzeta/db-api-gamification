9ea4772cdf4fef19bfb49122f0d2c4d6119390c1
User <User@DESKTOP-812UC42>
2021-12-02 00:34:22 -0300

    new adjustments 2

--- pages/api/tarefas.js
+++ pages/api/tarefas.js
@@ -1,10 +1,3 @@
-import Cookies from 'cookies';
-import Mysql from 'mysql';
-import Link from 'next/link';
-import SQL from 'sql-template-strings';
-import Layout from '../components/Layout';
-
-
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
