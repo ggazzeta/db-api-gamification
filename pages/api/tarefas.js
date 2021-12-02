2a50b316b0595af4dbc9b9c693f4abd3b0933cd5
User <User@DESKTOP-812UC42>
2021-12-02 00:25:36 -0300

    adjustments

---
+++ pages/api/tarefas.js
@@ -0,0 +1,26 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "sql10.freesqldatabase.com",
+    database: "sql10453712",
+    user: "sql10453712",
+    password: "hcjMA2wTW6"
+  });
+
+var tarefasResultado = [];
+
+con.connect(function(err) {
+    if (err) throw err;
+    con.query("SELECT NOME, DESCRICAO, FINALIZADA, (SELECT NOME FROM USUARIO U WHERE U.ID = T.USUARIO) NOMEUSUARIO, (SELECT NOME FROM TIMES TI WHERE TI.ID = T.TIME) NOMETIME FROM TAREFA T", function (err, result, fields) {
+      if (err) throw err;
+      tarefasResultado = result;
+    });
+  });
+
+ export default function tarefas(request, response){
+      const listaTarefas = [];
+
+      response.json({
+          listaTarefas: tarefasResultado
+    })
+  }
